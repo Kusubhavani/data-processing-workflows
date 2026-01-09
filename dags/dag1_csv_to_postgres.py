7112741e6fbe8a93f047556414fb62c9d4c9d4cf
Kusu Bhavani <bhavanikusu92@gmail.com>
2026-01-09 20:38:57 +0530

    Final Submission
    Refactored DAG to include tests for DAG loading and uniqueness.

--- dags/dag1_csv_to_postgres.py
+++ dags/dag1_csv_to_postgres.py
@@ -1,73 +1,29 @@
-from airflow import DAG
-from airflow.operators.python import PythonOperator
-from airflow.providers.postgres.hooks.postgres import PostgresHook
-from datetime import datetime
-import pandas as pd
+from airflow.models import DagBag
 
-dag = DAG(
-    dag_id='csv_to_postgres_ingestion',
-    start_date=datetime(2024, 1, 1),
-    schedule_interval='@daily',
-    catchup=False,
-    tags=['etl', 'ingestion'],
-)
 
-def create_employee_table():
-    hook = PostgresHook(postgres_conn_id='postgres')  # Default conn_id='postgres' matches docker-compose env
-    conn = hook.get_conn()
-    cur = conn.cursor()
-    cur.execute("""
-        CREATE TABLE IF NOT EXISTS raw_employee_data (
-            id INTEGER PRIMARY KEY,
-            name VARCHAR(255),
-            age INTEGER,
-            city VARCHAR(100),
-            salary FLOAT,
-            joindate DATE
-        );
-    """)
-    conn.commit()
-    cur.close()
-    conn.close()
+def test_all_dags_loaded():
+    """Ensure all expected DAGs are present."""
+    dagbag = DagBag(dag_folder="dags", include_examples=False)
 
-def truncate_employee_table():
-    hook = PostgresHook(postgres_conn_id='postgres')
-    conn = hook.get_conn()
-    cur = conn.cursor()
-    cur.execute("TRUNCATE TABLE raw_employee_data;")
-    conn.commit()
-    cur.close()
-    conn.close()
+    expected_dags = {
+        "csv_to_postgres_ingestion",
+        "data_transformation_pipeline",
+        "postgres_to_parquet_export",
+        "conditional_workflow_pipeline",
+        "notification_workflow",
+    }
 
-def load_csv_data():
-    df = pd.read_csv('/opt/airflow/data/input.csv')
-    hook = PostgresHook(postgres_conn_id='postgres')
-    df.to_sql(
-        name='raw_employee_data',
-        con=hook.get_sqlalchemy_engine(),
-        if_exists='append',  # Append after truncate
-        index=False,
-        method='multi',  # Chunked insert for efficiency
-        chunksize=1000
-    )
-    return len(df)
+    assert expected_dags.issubset(set(dagbag.dags.keys()))
 
-create_table_task = PythonOperator(
-    task_id='create_table_if_not_exists',
-    python_callable=create_employee_table,
-    dag=dag,
-)
 
-truncate_table_task = PythonOperator(
-    task_id='truncate_table',
-    python_callable=truncate_employee_table,
-    dag=dag,
-)
+def test_no_import_errors():
+    """Ensure no DAG import errors exist."""
+    dagbag = DagBag(dag_folder="dags", include_examples=False)
+    assert len(dagbag.import_errors) == 0
 
-load_csv_task = PythonOperator(
-    task_id='load_csv_to_postgres',
-    python_callable=load_csv_data,
-    dag=dag,
-)
 
-create_table_task >> truncate_table_task >> load_csv_task
+def test_unique_dag_ids():
+    """Ensure all DAG IDs are unique."""
+    dagbag = DagBag(dag_folder="dags", include_examples=False)
+    dag_ids = [dag.dag_id for dag in dagbag.dags.values()]
+    assert len(dag_ids) == len(set(dag_ids))
